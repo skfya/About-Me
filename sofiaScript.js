b3a86c4e02a1c8549b919106db1dd9543479fcf0
skfya <42008439+skfya@users.noreply.github.com>
2018-08-03 12:28:57 -0400

    Add files via upload

---
+++ sofiaScript.js
@@ -0,0 +1,59 @@
+function groups() {
+  if (document.getElementById("favGroups").style.display==="none") {
+    document.getElementById("favGroups").style.display="block";
+    document.getElementById("click").innerHTML="Hide more favorites";
+  } else {
+    document.getElementById("favGroups").style.display="none";
+    document.getElementById("click").innerHTML="Show more favorites";
+  }
+}
+
+function mxTwitter(){
+  if (document.getElementById("mon").style.display==="none") {
+    document.getElementById("mon").style.display="block";
+    document.getElementById("mxButton").innerHTML="Hide Twitter";
+  } else {
+    document.getElementById("mon").style.display="none";
+        document.getElementById("mxButton").innerHTML="Show Twitter";
+  }
+}
+
+function vixxTwitter(){
+  if (document.getElementById("vix").style.display==="none") {
+    document.getElementById("vix").style.display="block";
+    document.getElementById("vixxButton").innerHTML="Hide Twitter";
+  } else {
+    document.getElementById("vix").style.display="none";
+        document.getElementById("vixxButton").innerHTML="Show Twitter";
+  }
+}
+
+function svtTwitter(){
+  if (document.getElementById("17").style.display==="none") {
+    document.getElementById("17").style.display="block";
+    document.getElementById("svtButton").innerHTML="Hide Twitter";
+  } else {
+    document.getElementById("17").style.display="none";
+        document.getElementById("svtButton").innerHTML="Show Twitter";
+  }
+}
+
+function bapTwitter(){
+  if (document.getElementById("best").style.display==="none") {
+    document.getElementById("best").style.display="block";
+    document.getElementById("bapButton").innerHTML="Hide Twitter";
+  } else {
+    document.getElementById("best").style.display="none";
+        document.getElementById("bapButton").innerHTML="Show Twitter";
+  }
+}
+
+function inftTwitter(){
+  if (document.getElementById("infinite").style.display==="none") {
+    document.getElementById("infinite").style.display="block";
+    document.getElementById("inftButton").innerHTML="Hide Twitter";
+  } else {
+    document.getElementById("infinite").style.display="none";
+        document.getElementById("inftButton").innerHTML="Show Twitter";
+  }
+}
