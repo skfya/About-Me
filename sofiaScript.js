07490af2d44875a59c58afe2bd72e636f49257d9
skfya <42008439+skfya@users.noreply.github.com>
2018-08-03 12:28:21 -0400

    Delete sofiaScript.js

--- sofiaScript.js
+++
@@ -1,9 +0,0 @@
-function groups() {
-  if (document.getElementById("favGroups").style.display="none") {
-    document.getElementById("favGroups").style.display="block";
-    document.getElementById("click").innerHTML="Hide more favorites";
-  } else {
-    document.getElementById("favGroups").style.display="none";
-    document.getElementById("click").innerHTML="Show more favorites";
-    }
-}
